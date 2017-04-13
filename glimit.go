19c2550d76ef3d47a485e7737ce9b832d5be4c7b
Daniel Leining <daniel@leining.org>
2017-04-13 19:18:46 -0400

    Initial commit

---
+++ glimit.go
@@ -0,0 +1,114 @@
+package glimit
+
+import (
+	"errors"
+	"github.com/google/uuid"
+	"github.com/jinzhu/gorm"
+	"time"
+)
+
+var (
+	//ErrRateLimitExceeded Returned when a rate limit is exceeded
+	ErrRateLimitExceeded = errors.New("Rate Limit Exceeded")
+)
+
+//Limiter represents the rate limiter
+type Limiter struct {
+	db       *gorm.DB
+	ID       string
+	Times    int
+	Interval time.Duration
+}
+
+//Action represents the Action being ratelimited.
+type Action struct {
+	Timestamp time.Time `gorm:"index"`
+	ID        string
+	LimiterID string `gorm:"index"`
+}
+
+//DoMigrations adds the models to the database. Should be run once,
+// if you choose to use automigrations at all.
+func DoMigrations(db *gorm.DB) {
+	db.AutoMigrate(&Limiter{})
+	db.AutoMigrate(&Action{})
+}
+
+//NewLimiter Creates a new limiter with the specified arguments and saves it
+// to the database.
+func NewLimiter(actions int, interval time.Duration, db *gorm.DB) (*Limiter, error) {
+	newUUID, err := uuid.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+	newLimit := &Limiter{
+		db:       db,
+		ID:       newUUID.String(),
+		Times:    actions,
+		Interval: interval,
+	}
+	err = db.Save(newLimit).Error
+	if err != nil {
+		return nil, err
+	}
+	return newLimit, nil
+}
+
+//Take attemps to do an "action". if there is an error determining if an action is possible
+// or writing the action the database, the returned action count will be 0.
+// If the rate is exceeded, the total count will be returned, as well as ErrRateLimitExceeded.
+// If the Take is successful, it will return the amount of actions within the period,
+// and no errors.
+func (l *Limiter) Take() (int, error) {
+	var count int
+	calculated := time.Now().Truncate(l.Interval)
+	err := l.db.Model(&Action{}).Where("timestamp >= ? AND limiter_id = ?", calculated, l.ID).Count(&count).Error
+	if err != nil {
+		return 0, err
+	}
+	if count >= l.Times {
+		return count, ErrRateLimitExceeded
+	}
+	newActionID, err := uuid.NewUUID()
+	if err != nil {
+		return 0, err
+	}
+	newAction := &Action{
+		ID:        newActionID.String(),
+		Timestamp: time.Now(),
+		LimiterID: l.ID,
+	}
+	err = l.db.Save(&newAction).Error
+	if err != nil {
+		return 0, err
+	}
+	return count + 1, nil
+}
+
+//Cleanup deletes all "expired" actions associated with a limiter
+func (l *Limiter) Cleanup() error {
+	calculated := time.Now().Truncate(l.Interval)
+	err := l.db.Where("timestamp <= ? AND limiter_id = ?", calculated, l.ID).Delete(Action{}).Error
+	return err
+}
+
+//Delete deletes the limiter as well as all associated actions
+func (l *Limiter) Delete() error {
+	err := l.db.Delete(Action{}, &Action{LimiterID: l.ID}).Error
+	if err != nil {
+		return err
+	}
+	err = l.db.Delete(Limiter{}, &Limiter{ID: l.ID}).Error
+	return err
+}
+
+//ByID retrieves a limiter from it's ID
+func ByID(ID string, db *gorm.DB) (*Limiter, error) {
+	l := &Limiter{}
+	err := db.First(l, &Limiter{ID: ID}).Error
+	if err != nil {
+		return nil, err
+	}
+	l.db = db
+	return l, nil
+}
---
+++ glimit_test.go
@@ -0,0 +1,125 @@
+package glimit
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"testing"
+	"time"
+)
+
+var (
+	limiter *Limiter
+	db      *gorm.DB
+)
+
+func TestSetupDB(t *testing.T) {
+	var err error
+	db, err = gorm.Open("sqlite3", "testing.db")
+	if err != nil {
+		t.Error(err.Error())
+	}
+	db.LogMode(true)
+	DoMigrations(db)
+}
+
+func TestSetupLimiter(t *testing.T) {
+	var err error
+	limiter, err = NewLimiter(2, time.Second*5, db)
+	if err != nil {
+		t.Error(err.Error())
+	}
+}
+
+func TestTakeOne(t *testing.T) {
+	count, err := limiter.Take()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if count != 1 {
+		t.Error("Expected 1 got ", count)
+	}
+}
+
+func TestTakeTwo(t *testing.T) {
+	count, err := limiter.Take()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if count != 2 {
+		t.Error("Expected 2 got ", count)
+	}
+}
+
+func TestShouldExceed(t *testing.T) {
+	count, err := limiter.Take()
+	if err != ErrRateLimitExceeded {
+		t.Error("Was expecting error got ", err)
+	}
+	if count != 2 {
+		t.Error("was expecting 2 got ", count)
+	}
+}
+
+func TestShouldPass(t *testing.T) {
+	time.Sleep(6 * time.Second)
+	count, err := limiter.Take()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if count != 1 {
+		t.Error("was expecting 1 got ", count)
+	}
+}
+
+func TestCleanup(t *testing.T) {
+	var count int
+	err := limiter.Cleanup()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	err = db.Model(&Action{}).Where("limiter_id = ?", limiter.ID).Count(&count).Error
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if count != 1 {
+		t.Error("was expecting 1 got ", count)
+	}
+}
+
+func TestByID(t *testing.T) {
+	l, err := ByID(limiter.ID, db)
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if l.Interval != limiter.Interval {
+		t.Fail()
+	}
+	if l.Times != limiter.Times {
+		t.Fail()
+	}
+	if l.ID != limiter.ID {
+		t.Fail()
+	}
+}
+
+func TestDelete(t *testing.T) {
+	err := limiter.Delete()
+	if err != nil {
+		t.Error(err.Error())
+	}
+	_, err = ByID(limiter.ID, db)
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestEnsureNoActions(t *testing.T) {
+	var count int
+	err := db.Model(&Action{}).Where(&Action{LimiterID: limiter.ID}).Count(&count).Error
+	if err != nil {
+		t.Error(err.Error())
+	}
+	if count != 0 {
+		t.Error("Expected 0 got ", count)
+	}
+}
